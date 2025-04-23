8193141590fb849eb9b41554fe63246c1b4a889c
Adrian gutierrez <19gutierreza@gmail.com>
2025-04-23 16:18:01 +0900

    added gmap structure

---
+++ include/cub3d.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrgutie <adrgutie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 16:02:18 by adrgutie          #+#    #+#             */
+/*   Updated: 2025/04/23 16:17:14 by adrgutie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+//map_structure
+//player_starting_direction is N, S, E or W
+typedef struct s_gmap
+{
+	char	**map;
+	char	*n_wall_path_texture;
+	char	*s_wall_path_texture;
+	char	*w_wall_path_texture;
+	char	*e_wall_path_texture;
+	int		floor_color_rgb[3];
+	int		ceiling_color_rgb[3];
+	int		player_starting_x_pos;
+	int		player_starting_y_pos;
+	char	player_starting_direciton;
+}	t_gmap;
+
+#endif
